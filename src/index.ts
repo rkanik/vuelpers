1465d6cc25e37efec021ad86b563e76f4032f0a8
encoderitanik <anik@encoderit.net>
2021-12-02 19:02:14 +0600

    added mutations and createApi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import axios from 'axios'
 
 export { _ }
 
@@ -238,4 +239,260 @@ export const toFormData = (value: any, extra: object = {}) => {
 	}
 	traverse(data)
 	return formData
+}
+
+const toSuccess = (res: any) => {
+	if (res.status === 'error') return toError(res)
+	let mRes = { ...res }
+	delete mRes.code
+	delete mRes.status
+	let data = {
+		...mRes,
+		statusCode: res.code,
+		statusText: res.status,
+		message: res.message || 'Request succeeded'
+	}
+	return data
+}
+
+const toError = (error: any) => {
+	let data: any = {}
+	if (error.response) {
+		const res = error.response
+		data = {
+			...data,
+			statusCode: res.status,
+			statusText: res.statusText || res.data.status,
+		}
+		if (res.data.errors) data.errors = Object
+			.entries(res.data.errors)
+			.reduce((acc, [key, value]) => ({
+				...acc, [key]: value
+			}), {})
+		data.message = res.data.message || res.message
+		data.ref = error.response
+	}
+	else {
+		data.statusCode = error.status
+		data.statusText = error.statusText
+		data.message = error.data.message
+		if (error.data.errors) data.errors = error.data.errors
+	}
+	data.message = data.message || error.message
+	return data
+}
+
+const handler = (requestPromise: Promise<any>) => {
+	return new Promise(resolve => {
+		requestPromise
+			.then(res => {
+				resolve([
+					null, convertKeysToCamelCase(
+						toSuccess(res.data)
+					)])
+			})
+			.catch(error => {
+				resolve([
+					convertKeysToCamelCase(
+						toError(error.response || error)
+					)])
+			})
+	})
+}
+
+export const createAPI = (config: { baseURL: string }) => {
+	const api = axios.create({ baseURL: config.baseURL })
+	return {
+		setHeaders(headers: object) {
+			api.defaults.headers.common = {
+				...api.defaults.headers.common,
+				...headers
+			}
+		},
+		get(endpoint: string, query = {}) {
+			return handler(
+				api.get(
+					endpoint, {
+					params: convertKeysToSnakeCase(query)
+				})
+			)
+		},
+		post(endpoint: string, body: object, query = {}) {
+			return handler(
+				api.post(
+					endpoint,
+					convertKeysToSnakeCase(body), {
+					params: convertKeysToSnakeCase(query)
+				})
+			)
+		},
+		put(endpoint: string, body: object, query = {}) {
+			return handler(
+				api.put(
+					endpoint,
+					convertKeysToSnakeCase(body), {
+					params: convertKeysToSnakeCase(query)
+				})
+			)
+		},
+		patch(endpoint: string, body: object, query = {}) {
+			return handler(
+				api.patch(
+					endpoint,
+					convertKeysToSnakeCase(body), {
+					params: convertKeysToSnakeCase(query)
+				})
+			)
+		},
+		delete(endpoint: string) {
+			return handler(
+				api.delete(endpoint)
+			)
+		}
+	}
+}
+
+const mutations: any = {
+	SET(state: any, payload: any) {
+		if (!_.isPlainObject(payload)) {
+			throw Error('Payload have to be an object')
+		}
+		Object
+			.entries(payload)
+			.forEach(([path, value]) => {
+				value = typeof value === 'function'
+					? value(_.get(state, path))
+					: value
+				_.set(state, path, value)
+			})
+	},
+	RESET(state: any, initialState: any) {
+		mutations.SET(state, initialState)
+	},
+	PUSH(state: any, payload: any) {
+		if (Array.isArray(payload)) {
+			let [path, ...items] = payload
+			let target = _.get(state, path)
+
+			if (!target || !Array.isArray(target)) {
+				throw Error('Specified state path not found or property is not an array')
+			}
+
+			target.push(...items)
+		}
+		else if (_.isPlainObject(payload)) {
+			Object.entries(payload).forEach(([path, item]) => {
+				let target = _.get(state, path)
+
+				if (!target || !Array.isArray(target)) {
+					throw Error('Specified state path not found or property is not an array')
+				}
+
+				let items = Array.isArray(item) ? item : [item]
+				target.push(...items)
+			})
+		}
+		else throw Error('Invalid payload type.')
+	},
+	UNSHIFT(state: any, payload: any) {
+		if (Array.isArray(payload)) {
+			let [path, ...items] = payload
+			let target = _.get(state, path)
+
+			if (!target || !Array.isArray(target)) {
+				throw Error('Specified state path not found or property is not an array')
+			}
+
+			target.unshift(...items)
+		}
+		else if (_.isPlainObject(payload)) {
+			Object.entries(payload).forEach(([path, item]) => {
+				let target = _.get(state, path)
+
+				if (!target || !Array.isArray(target)) {
+					throw Error('Specified state path not found or property is not an array')
+				}
+
+				let items = Array.isArray(item) ? item : [item]
+				target.unshift(...items)
+			})
+		}
+		else throw Error('Invalid payload type.')
+	},
+	CONCAT(state: any, [path, items]: any) {
+		let target = _.get(state, path)
+
+		if (!target || !Array.isArray(target)) {
+			throw Error('Specified state path not found or property is not an array')
+		}
+
+		_.set(state, path, target.concat(items))
+	},
+	DELETE(state: any, [path, key, match = 'id']: any) {
+		let target = _.get(state, path)
+
+		if (!target || !Array.isArray(target)) {
+			throw Error('Specified state path not found or property is not an array')
+		}
+
+		let keys = Array.isArray(key) ? key : [key]
+		_.set(state, path, _.get(state, path).filter(
+			(el: any) => !keys.includes(el[match])
+		))
+	},
+	UPDATE(state: any, [path, data, match = 'id']: any) {
+		let target = _.get(state, path)
+
+		if (!target || !Array.isArray(target)) {
+			throw Error('Specified state path not found or property is not an array')
+		}
+
+		_.set(state, path, _.get(state, path).map((el: any) => {
+			return _.get(el, match) === _.get(data, match) ? data : el
+		}))
+	},
+	MERGE(state: any, [path, items, match = 'id']: any) {
+		let target = _.get(state, path)
+
+		if (!target || !Array.isArray(target)) {
+			throw Error('Specified state path not found or property is not an array')
+		}
+
+		items.forEach((item: any) => {
+			let index = target.findIndex((a: any) => _.get(a, match) === _.get(item, match))
+			if (index !== -1) target.splice(index, 1, item)
+			else target.unshift(item)
+		})
+	}
+}
+
+export const createMutations = (...types: any[]) => {
+	if (!types.length) return { ...mutations }
+	return Object
+		.keys(mutations)
+		.filter(name => types.includes(name))
+		.reduce((m, a) => ({ ...m, [a]: mutations[a] }), {})
+};
+
+export const createGetters = (...getters: any[]) => {
+	return getters.reduce((getters, name) => {
+		if (typeof name === 'string') {
+			getters[`$${name}`] = (state: any) => state[name]
+		}
+		if (_.isPlainObject(name)) Object.entries(name).forEach(([key, path]: any[]) => {
+			getters[`$${key}`] = (state: any) => _.get(state, path)
+		})
+		return getters
+	}, {})
+}
+
+export const handleAction = (apiRequestPromise: Promise<any>, successCallback: any, errorCallback: any) => {
+	return new Promise(resolve => {
+		apiRequestPromise
+			.then(([error, response]) => {
+				if (!error && _.isFunction(successCallback)) successCallback(response)
+				if (error && _.isFunction(errorCallback)) errorCallback(error)
+				resolve([error, response])
+			})
+	})
 }
