549f4190d5ac5c84db3fd98e5d6fe6cb05066de7
RK Anik <rkanik.me@gmail.com>
2021-12-14 00:17:10 +0600

    getMS added in consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,13 @@
+export type TimeUnit = 'SEC' | 'MIN' | 'HOUR' | 'DAY' | 'WEEK' | 'MONTH' | 'YEAR'
 export const TIME = {
-	SECOND: 1000,
-	MINUTE: 1000 * 60,
+	SEC: 1000,
+	MIN: 1000 * 60,
 	HOUR: 1000 * 60 * 60,
 	DAY: 1000 * 60 * 60 * 24,
 	WEEK: 1000 * 60 * 60 * 24 * 7,
 	MONTH: 1000 * 60 * 60 * 24 * 30,
-	YEAR: 1000 * 60 * 60 * 24 * 365
+	YEAR: 1000 * 60 * 60 * 24 * 365,
+	getMS(n: number, unit: TimeUnit) {
+		return n * this[unit]
+	},
 }
