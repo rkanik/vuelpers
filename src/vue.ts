4126221003e9b9a4532049816b2fc486b58e9c13
encoderitanik <anik@encoderit.net>
2022-02-10 16:42:02 +0600

    Added getRouteMeta

--- src/vue.ts
+++ src/vue.ts
@@ -1,4 +1,4 @@
-import { camelCase, upperFirst, isFunction } from 'lodash'
+import { camelCase, upperFirst, isFunction } from "lodash";
 
 /**
  * @example
@@ -8,18 +8,29 @@ import { camelCase, upperFirst, isFunction } from 'lodash'
  *      /[A-Z]\w+\.(vue|js|ts)$/
  *    ), Vue
  * )
- * 
+ *
  * @param context - Require context
  * @param Vue - Vue instance
  */
 export const registerComponents = (context: any, Vue: any) => {
   context.keys().forEach((filename: string) => {
-    const component = context(filename).default || context(filename)
+    const component = context(filename).default || context(filename);
     const componentName = !isFunction(component)
-      ? component.name : upperFirst(camelCase(
-        (filename.replace(/^\.\/(.*)\.\w+$/, '$1'))
-          .split('/').pop()
-      ))
-    Vue.component(componentName, component)
+      ? component.name
+      : upperFirst(
+          camelCase(
+            filename
+              .replace(/^\.\/(.*)\.\w+$/, "$1")
+              .split("/")
+              .pop()
+          )
+        );
+    Vue.component(componentName, component);
   });
-}
+};
+
+export const getRouteMeta = (route: any, key: string) => {
+  const matchedRoute = route.matched.find((route: any) => route.meta[key]);
+  if (!matchedRoute) return null;
+  return matchedRoute.meta[key];
+};
