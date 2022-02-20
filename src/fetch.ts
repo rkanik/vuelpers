cffa2cef6d6f6123c99bdaed20732fb82644e2a9
encoderitanik <anik@encoderit.net>
2022-02-20 10:41:04 +0600

    Added indivisual case converting

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,57 +1,80 @@
-import { jsonParse } from './common'
-import {
-  convertKeysToCamelCase,
-  convertKeysToSnakeCase
-} from './objects'
+import { jsonParse } from "./common";
+import { queryString } from "./query-string";
+import { convertKeysToCamelCase, convertKeysToSnakeCase } from "./objects";
 
-import {
-  queryString
-} from './query-string'
-
-type Cases = 'camelCase' | 'snake_case'
+type Cases = "camelCase" | "snake_case" | null;
 
 type FetchAPIConfig = {
-  baseURL: string,
-  apiCase?: Cases
-}
+  baseURL: string;
+  response: {
+    convertCase: Cases;
+  };
+  request: {
+    convertCase: Cases;
+  };
+};
 
 export type FetchResponse = {
-  status: number | 'net'
-  statusText: string
-  message?: string
-  [key: string]: any
-}
+  status: number | "net";
+  statusText: string;
+  message?: string;
+  [key: string]: any;
+};
 
 export class FetchAPI {
-  private headers: any
-  private baseURL: string
-  private apiCase: Cases = 'snake_case'
+  private headers: any;
+  private baseURL: string;
   private init: RequestInit = {
-    mode: 'cors',
-    cache: 'default',
+    mode: "cors",
+    cache: "default",
+  };
+
+  private config: FetchAPIConfig = {
+    baseURL: "",
+    request: {
+      convertCase: null,
+    },
+    response: {
+      convertCase: null,
+    },
+  };
+
+  constructor(config: Partial<FetchAPIConfig>, init?: RequestInit) {
+    this.baseURL = config.baseURL || "";
+
+    // CONFIG
+    this.config = {
+      ...this.config,
+      baseURL: config.baseURL || this.config.baseURL,
+    };
+    if (config.request && config.request.convertCase) {
+      this.config.request.convertCase = config.request.convertCase;
+    }
+    if (config.response && config.response.convertCase) {
+      this.config.response.convertCase = config.response.convertCase;
+    }
+
+    if (init) this.init = { ...this.init, ...init };
+    this.headers = { Accept: "application/json" };
   }
 
-  constructor(config: FetchAPIConfig, init?: RequestInit) {
-    this.baseURL = config.baseURL
-
-    if (init) this.init = { ...this.init, ...init }
-    if (config.apiCase) this.apiCase = config.apiCase
-
-    this.headers = { 'Accept': 'application/json' }
+  private ensureCase(data: any, caseType: Cases) {
+    if (caseType === "camelCase") return convertKeysToCamelCase(data);
+    if (caseType === "snake_case") return convertKeysToSnakeCase(data);
+    return data;
   }
 
   private getUrl(endpoint: string, query?: object) {
     // If there is no query
-    if (!query) return `${this.baseURL}${endpoint}`
+    if (!query) return `${this.baseURL}${endpoint}`;
 
     // converting query to snake case if api is in snake case
-    if (this.apiCase === 'snake_case') {
-      query = convertKeysToSnakeCase(query)
-    }
+    query = this.ensureCase(query, this.config.request.convertCase);
 
     // returning formatted url
-    return `${this.baseURL}${endpoint}?${queryString.stringify(query as object)}`
-
+    return `${this.baseURL}${endpoint}?${queryString.stringify(
+      query as object
+    )}`;
   }
 
   private handleFetch(
@@ -73,7 +96,6 @@ export class FetchAPI {
 
           // Parsing error text
           return raw.text().then((text) => {
-
             // Parsing error text
             let [err, parsedObject] = jsonParse(text);
 
@@ -81,14 +103,13 @@ export class FetchAPI {
             if (err) throw new Error(text);
 
             // Erorr body is object
-            return parsedObject
+            return parsedObject;
           });
         })
         .then((json) => {
           // Converting to camelCase if api is in snake_case
-          if (this.apiCase === 'snake_case') {
-            json = convertKeysToCamelCase(json)
-          }
+          json = this.ensureCase(json, this.config.response.convertCase);
+
           // Sending success response
           response = { ...response, ...json };
           return resolve([false, response]);
@@ -102,104 +123,96 @@ export class FetchAPI {
   }
 
   private getHeaders() {
-    const headers = new Headers()
-    Object
-      .entries(this.headers)
-      .forEach(([key, value]: any) => {
-        headers.append(key, value)
-      })
-    return headers
+    const headers = new Headers();
+    Object.entries(this.headers).forEach(([key, value]: any) => {
+      headers.append(key, value);
+    });
+    return headers;
   }
 
   public setHeaders(headers: object) {
     this.headers = {
       ...this.headers,
-      ...headers
-    }
+      ...headers,
+    };
   }
 
   public removeHeaders(...keys: string[]) {
-    keys.forEach(key => {
-      delete this.headers[key]
-    })
+    keys.forEach((key) => {
+      delete this.headers[key];
+    });
   }
 
   public get(endpoint: string, query?: object) {
-    const input = this.getUrl(endpoint, query)
-    const headers = this.getHeaders()
+    const input = this.getUrl(endpoint, query);
+    const headers = this.getHeaders();
     return this.handleFetch(
       fetch(input, {
         headers,
-        method: 'GET',
+        method: "GET",
       })
-    )
+    );
   }
 
   public post(endpoint: string, body: any, query?: object) {
-    const input = this.getUrl(endpoint, query)
-    const headers = this.getHeaders()
+    const input = this.getUrl(endpoint, query);
+    const headers = this.getHeaders();
     if (!(body instanceof FormData)) {
-      if (this.apiCase === 'snake_case') {
-        body = convertKeysToSnakeCase(body)
-      }
-      body = JSON.stringify(body)
-      headers.append('Content-Type', 'application/json')
+      body = this.ensureCase(body, this.config.request.convertCase);
+      body = JSON.stringify(body);
+      headers.append("Content-Type", "application/json");
     }
     return this.handleFetch(
       fetch(input, {
         body,
         headers,
-        method: 'POST',
+        method: "POST",
       })
-    )
+    );
   }
 
   public patch(endpoint: string, body: any, query?: object) {
-    const input = this.getUrl(endpoint, query)
-    const headers = this.getHeaders()
+    const input = this.getUrl(endpoint, query);
+    const headers = this.getHeaders();
     if (!(body instanceof FormData)) {
-      if (this.apiCase === 'snake_case') {
-        body = convertKeysToSnakeCase(body)
-      }
-      body = JSON.stringify(body)
-      headers.append('Content-Type', 'application/json')
+      body = this.ensureCase(body, this.config.request.convertCase);
+      body = JSON.stringify(body);
+      headers.append("Content-Type", "application/json");
     }
     return this.handleFetch(
       fetch(input, {
         body,
         headers,
-        method: 'PATCH',
+        method: "PATCH",
       })
-    )
+    );
   }
 
   public put(endpoint: string, body: any, query?: object) {
-    const input = this.getUrl(endpoint, query)
-    const headers = this.getHeaders()
+    const input = this.getUrl(endpoint, query);
+    const headers = this.getHeaders();
     if (!(body instanceof FormData)) {
-      if (this.apiCase === 'snake_case') {
-        body = convertKeysToSnakeCase(body)
-      }
-      body = JSON.stringify(body)
-      headers.append('Content-Type', 'application/json')
+      body = this.ensureCase(body, this.config.request.convertCase);
+      body = JSON.stringify(body);
+      headers.append("Content-Type", "application/json");
     }
     return this.handleFetch(
       fetch(input, {
         body,
         headers,
-        method: 'PUT',
+        method: "PUT",
       })
-    )
+    );
   }
 
   public delete(endpoint: string) {
-    const input = this.getUrl(endpoint)
-    const headers = this.getHeaders()
+    const input = this.getUrl(endpoint);
+    const headers = this.getHeaders();
     return this.handleFetch(
       fetch(input, {
         headers,
-        method: 'DELETE',
+        method: "DELETE",
       })
-    )
+    );
   }
 }
