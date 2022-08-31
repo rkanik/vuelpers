ffc69fe9415cca46ad0b5153d9549e863da5c633
encoderitanik <anik@encoderit.net>
2022-08-31 15:50:44 +0600

    Added queryString set

--- src/query-string.ts
+++ src/query-string.ts
@@ -1,21 +1,30 @@
-import { get } from "lodash";
+import { get, isString } from 'lodash'
+import { changeLocationQuery } from './browser'
+import { hasWindow } from './common'
 
 export const queryString = {
-  get(key: string) {
-    return get(this.parse(), key)
-  },
-  parse(query: string = location.search.substring(1)) {
-    return Object.fromEntries(
-      new URLSearchParams(query)
-    )
-  },
-  stringify(object: object) {
-    const params = new URLSearchParams();
-    Object
-      .entries(object)
-      .forEach(([key, value]: [string, any]) => {
-        params.append(key, value)
-      })
-    return params.toString()
-  }
+	get(key: string) {
+		return get(this.parse(), key)
+	},
+	parse(query?: string) {
+		if (!hasWindow()) return null
+		query = location.search.substring(1)
+		return Object.fromEntries(new URLSearchParams(query))
+	},
+	stringify(v: object) {
+		const params = new URLSearchParams()
+		Object.entries(v).forEach(([key, value]: [string, any]) => {
+			params.append(key, value)
+		})
+		return params.toString()
+	},
+	set(key: string | { [key: string]: any }, value: any) {
+		const params = isString(key) ? { [key]: value } : key
+		return changeLocationQuery(
+			queryString.stringify({
+				...(queryString.parse() || {}),
+				...params,
+			})
+		)
+	},
 }
